a7775be8a49a532a78d9f759b17b963bae636470
Michael Bock <me@thought2.de>
2021-06-26 13:15:46 +0200

    Finish main features

--- examples/index.ts
+++ examples/index.ts
@@ -4,8 +4,13 @@ type A0 = _.Empty<number, string>
 
 type A1 = _.Insert<2, 'a', A0>
 
-type A2 = _.Insert<6, 'b', A1>
+type A2 = _.Insert<2, 'b', A1>
 
 type A3 = _.Delete<6, A2>
 
 type A4 = _.Set<2, '', A3>
+
+type B0 = _.FromEntries<
+  [[4, ''], [4, 'abc'], [2, 'sss'], [33, '5'], [34, 'ooo']],
+  [number, string]
+>
--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,10 @@ import { pipe } from 'fp-ts/function'
 export type Map<K = any, V = any> = {
   _K: K
   _V: V
-  data: [any, any][]
+  data: Entry[]
 } & MapBrand
+
+type Entry<K = any, V = any> = [K, V]
 
 /**
  * It's a greeting
@@ -24,11 +26,13 @@ export type Map<K = any, V = any> = {
  * @since 1.0.0
  * @category Greetings
  */
-export type Empty<K = any, V = any> = UnsafeMap<K, V, []>
+export type Empty<K = any, V = any> = UnsafeMap<K, V, Empty_>
+
+type Empty_ = []
 
 type MapBrand = { readonly Map: unique symbol }
 
-type UnsafeMap<K, V, D extends [any, any][]> = {
+type UnsafeMap<K, V, D extends Entry[]> = {
   _K: K
   _V: V
   data: D
@@ -48,8 +52,16 @@ export type Insert<
   K extends KeyOf<M>,
   V extends ValOf<M>,
   M extends Map
-> = Exists<K, M> extends false
-  ? UnsafeMap<KeyOf<M>, ValOf<M>, [[K, V], ...M['data']]>
+> = Insert_<K, V, DataOf<M>> extends infer result
+  ? result extends Entry[]
+    ? UnsafeMap<KeyOf<M>, ValOf<M>, result>
+    : `ERROR: Key ${Print<K>} already exists`
+  : never
+
+type Print<T> = T extends number ? `${T}` : T extends string ? T : '...'
+
+type Insert_<K, V, D extends Entry[]> = Exists_<K, D> extends false
+  ? [[K, V], ...D]
   : unknown
 
 export type Set<
@@ -58,21 +70,34 @@ export type Set<
   M extends Map
 > = UnsafeMap<KeyOf<M>, ValOf<M>, Set_<K, V, DataOf<M>>>
 
+/**
+ * It's a greeting
+ *
+ * @since 1.0.0
+ * @category Greetings
+ * @example
+ *   import { FromEntries } from 'ts-typelevel-maps'
+ *
+ *   type People = FromEntries<
+ *     [[894, 'Michael'], [23, 'Tom'], [33, 'Linda'], [34, 'Maria']],
+ *     [number, string]
+ *   >
+ *
+ *   type T = Set<2, 'ABC', People>
+ */
 type Set_<K, V, D> = D extends [[infer k, infer v], ...infer tail]
   ? Is<k, K> extends true
     ? [[k, V], ...Set_<K, V, tail>]
     : [[k, v], ...Set_<K, V, tail>]
   : []
 
-export type Lookup<K extends KeyOf<M>, M extends Map> = Lookup_<K, M>
+export type Lookup<K extends KeyOf<M>, M extends Map> = Lookup_<K, DataOf<M>>
 
-type Lookup_<K, M> = M extends Map
-  ? DataOf<M> extends [[infer k, infer v], ...infer tail]
-    ? Is<k, K> extends true
-      ? v
-      : Lookup_<K, tail>
-    : unknown
-  : never
+type Lookup_<K, D> = D extends [[infer k, infer v], ...infer tail]
+  ? Is<k, K> extends true
+    ? v
+    : Lookup_<K, tail>
+  : unknown
 
 export type Delete<K extends KeyOf<M>, M extends Map> = UnsafeMap<
   KeyOf<M>,
@@ -86,15 +111,32 @@ type Delete_<K, D> = D extends [[infer k, infer v], ...infer tail]
     : [[k, v], ...Delete_<K, tail>]
   : []
 
-export type Exists<K extends KeyOf<M>, M extends Map> = Not<
-  Is<Lookup<K, M>, unknown>
->
+export type Exists<K extends KeyOf<M>, M extends Map> = Exists_<K, DataOf<M>>
+
+type Exists_<K, D extends Entry[]> = Not<Is<Lookup_<K, D>, unknown>>
 
 export type KeyOf<M extends Map> = M['_K']
 
 export type ValOf<M extends Map> = M['_V']
 
 export type DataOf<M extends Map> = M['data']
+
+export type FromEntries<
+  Es extends Entry<KV[0], KV[1]>[],
+  KV extends [any, any]
+> = FromEntries_<Es, KV[0], KV[1]>
+
+type FromEntries_<Es, K, V> = Es extends [[infer k, infer v], ...infer tail]
+  ? FromEntries_<tail, K, V> extends infer result
+    ? result extends Map<K, V>
+      ? k extends K
+        ? v extends V
+          ? Insert<k, v, result>
+          : never
+        : never
+      : result
+    : never
+  : Empty
 
 // -----------------------------------------------------------------------------
 // Internal
