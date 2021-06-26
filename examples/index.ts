8f73541b0d5574451504c9633536df69a8588288
Michael Bock <me@thought2.de>
2021-06-26 19:33:08 +0200

    Fix example

--- examples/index.ts
+++ examples/index.ts
@@ -1,18 +1,18 @@
 import * as _ from '../src'
 
-type A0 = _.Empty<number, string>
+export type A0 = _.Empty<number, string>
 
-type A1 = _.Upsert<2, 'a', A0>
+export type A1 = _.Upsert<2, 'a', A0>
 
-type A2 = _.Upsert<4, 'b', A1>
+export type A2 = _.Upsert<4, 'b', A1>
 
-type A3 = _.Delete<6, A2>
+export type A3 = _.Delete<6, A2>
 
-type A4 = _.Upsert<2, '', A3>
+export type A4 = _.Upsert<2, '', A3>
 
-type B0 = _.FromEntries<
+export type B0 = _.FromEntries<
   [[44, ''], [4, 'abc'], [2, 'sss'], [33, '5'], [34, 'ooo']],
   [number, string]
 >
 
-type R = B0['data'][3]
+export type R = B0['data'][3]
